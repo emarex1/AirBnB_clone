75600a6df134d0fcec0358752107553aa2d26ff4
emarex1 <emmanuelawoh0@gmail.com>
2023-03-09 08:02:56 +0100

    first task

---
+++ models/base_model.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+import uuid
+from datetime import datetime
+
+
+class BaseModel:
+    """Defines all common attributes/methods for other classes"""
+
+    def __init__(self):
+        """Initialize instance of BaseModel"""
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+
+    def __str__(self):
+        """Returns string representation of BaseModel instance"""
+        class_name = self.__class__.__name__
+        return "[{}] ({}) {}".format(class_name, self.id, self.__dict__)
+
+    def save(self):
+        """Updates the public instance attribute updated_at with the current datetime"""
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        """Returns a dictionary containing all keys/values of __dict__ of the instance"""
+        dict_copy = self.__dict__.copy()
+        dict_copy['__class__'] = self.__class__.__name__
+        dict_copy['created_at'] = self.created_at.isoformat()
+        dict_copy['updated_at'] = self.updated_at.isoformat()
+        return dict_copy
