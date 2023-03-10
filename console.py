319e4aa17ade859040070c288ce9f452122501f2
emarex1 <emmanuelawoh0@gmail.com>
2023-03-10 13:49:28 +0100

    console.py

---
+++ console.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""Entry point of the command interpreter"""
+
+import cmd
+
+
+class HBNBCommand(cmd.Cmd):
+    """Command interpreter class"""
+
+    prompt = "(hbnb) "
+
+    def do_quit(self, arg):
+        """Quit command to exit the program"""
+        return True
+
+    def do_EOF(self, arg):
+        """EOF command to exit the program"""
+        return True
+
+    def emptyline(self):
+        """Empty line should not execute anything"""
+        pass
+
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
+
